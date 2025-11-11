25bd5443ad32f60768e3496b66040447be122e4d
ILSA1414 <bjakkm@gmail.com>
2025-11-11 15:47:47 +0900

    카드 벌어지는 오류 수정
    이제 카드 내서 패 줄어들때도 HandManager의 rePlaceCard 쓰면 됨

--- DeathBoard/Assets/02.Scripts/cardPreset/HandManager.cs
+++ DeathBoard/Assets/02.Scripts/cardPreset/HandManager.cs
@@ -6,8 +6,8 @@ public class HandManager : MonoBehaviour
     public int NumOfHand = 0;
     public List<GameObject> hand = new List<GameObject>();
     public GameObject Pos1;
-    
-    
+
+    private readonly float PosX = 5.225f;
     private readonly float moveX = -1.3f * 0.12f; //카드가 추가될 때마다 좌측으로 1.3만큼 이동
     private readonly float PosY = 2.3f;
     private readonly float PosZ = -1.8f; //카드의 Y, Z축은 카드 매수와 관계없이 고정
@@ -25,37 +25,35 @@ public class HandManager : MonoBehaviour
     }
     public Vector3 PlaceNewCard()
     {
-        Vector3 newCardPosition = new Vector3(NumOfHand * (moveX * (-1)) + Pos1.transform.position.x, PosY, PosZ - (0.001f * NumOfHand));
+        Vector3 newCardPosition = new Vector3(NumOfHand * (moveX * (-1)) + Pos1.transform.position.x, PosY, PosZ - (0.001f * NumOfHand)); ; //카드의 가로 길이는 1
         return newCardPosition;
     }
     public void HandPlus(GameObject newCard)
     {
         this.NumOfHand++;
         hand.Add(newCard);
-        rePlaceCard();
     }
     public void rePlaceCard()
     {
-        for (int i = 0; i < hand.Count; i++)
+        for (int i = 0; i < hand.Count-1; i++)
         {
             GameObject thisCard = hand[i];
             CardReaction reactionScript = thisCard.GetComponent<CardReaction>();
+            Vector3 position = reactionScript.originalPosition;
             Vector3 targetPosition = new Vector3(
-                Pos1.transform.position.x + i * moveX, 
+                ((hand.Count-1) - i*2)*moveX + PosX,
                 PosY,
                 PosZ - (0.001f * i)
             );
             if (reactionScript.zoomIn == false)
             {
-
-                Vector3 position = thisCard.transform.localPosition;
-                position.x += moveX;
+                position = targetPosition;
                 thisCard.transform.position = position;
             }
             else
             {
-                Vector3 position = reactionScript.originalPosition;
-                position.x += moveX;
+
+                position = targetPosition;
                 thisCard.transform.localPosition = position;
                 reactionScript.originalPosition = position;
             }
