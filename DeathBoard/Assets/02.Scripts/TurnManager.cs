2f5c6c81e6891072bd1216bd865293dbc601f8e7
ILSA1414 <bjakkm@gmail.com>
2025-11-16 06:41:20 +0900

    .

---
+++ DeathBoard/Assets/02.Scripts/TurnManager.cs
@@ -0,0 +1,153 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class TurnManager : MonoBehaviour
+{   
+    public CardManager cardmanager;
+    public EnemyCardManager enemycardmanager;
+    public static int currentturn = 1;
+    public static bool turnend = false;
+    public MoveManager movemanager;
+    public BattleManager battlemanager;
+    public EnemyFieldManager enemyFieldManager;
+    public EffectManager effectManager;
+    public FieldManager fieldManager;
+    //현재 턴(1->카드 드로우 단계, 2->카드 내려놓기 단계, 3->적카드 내려놓기 단계, 4->카드 효과실행 단계, 5->공격단계,6->뒷열 카드 전진 단계, 7->카드 효과실행 단계2, 8->공격단계2, 9->뒷열 카드 전진 단계2, 10->총격 선택, 11->상대방 검사 선택)
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i=0;i<2;i++)
+        {
+            cardmanager.DrawHand();
+            enemycardmanager.DrawHand();
+        }
+        
+        NextTurn();
+    }
+
+    // Update is called once per frame
+    
+    public void NextTurn(){
+        turnend = false;
+        if(currentturn == 1)//드로우턴
+        {
+            cardmanager.DrawHand();
+            enemycardmanager.DrawHand();
+            currentturn = 2;
+            turnend = true;
+        }
+        else if(currentturn == 2)//카드내려놓기
+        {
+            
+        }
+        else if(currentturn == 3)//적카드내려놓기
+        {
+            enemyFieldManager.EnemyFieldSet();
+            currentturn = 4;
+            turnend = true;
+        }
+        else if(currentturn == 4)//카드효과실행
+        {
+            
+            for(int i=1;i>=0;i--)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    if(fieldManager.CurrntField[i,j] != null)
+                    {
+                        Debug.LogWarning("여기에 존재");
+                        Debug.Log(i);
+                        Debug.Log(j);
+                        effectManager.EffectCast(fieldManager.CurrntField[i,j].Value,3);
+                    }
+                }
+            }
+            for(int i=2;i<4;i++)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    if(fieldManager.CurrntField[i,j] != null)
+                    {
+                        effectManager.EnemyEffectCast(fieldManager.CurrntField[i,j].Value,3);
+                    }
+                }
+            }
+            currentturn = 5;
+            turnend = true;
+        }
+        else if(currentturn == 5)//공격단계
+        {
+            currentturn = 6; 
+            battlemanager.StartBattle();
+            turnend = true;
+                
+        }
+        else if(currentturn == 6)//뒷열카드전진
+        {
+            currentturn = 7;   
+            movemanager.StartMoveTurn();
+            turnend = true;
+        }
+        else if(currentturn == 7)//효과실행2
+        {
+<<<<<<< HEAD
+            //currentturn = 8;
+
+=======
+            
+            for(int i=1;i>=0;i--)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    if(fieldManager.CurrntField[i,j] != null)
+                    {
+                        Debug.LogWarning(fieldManager.CurrntField[i,j].Value);
+                        effectManager.EffectCast(fieldManager.CurrntField[i,j].Value,3);
+                    }
+                }
+            }
+            for(int i=2;i<4;i++)
+            {
+                for(int j=0;j<7;j++)
+                {
+                    if(fieldManager.CurrntField[i,j] != null)
+                    {
+                        effectManager.EnemyEffectCast(fieldManager.CurrntField[i,j].Value,3);
+                    }
+                }
+            }
+            currentturn = 8;
+>>>>>>> e148a1e0d4fef6a49bbbc48fc361afb348430a1b
+            turnend = true;
+        }
+        else if (currentturn == 8)//공격단계2
+        {
+            currentturn = 9;
+            battlemanager.StartBattle();
+            turnend = true;
+        }
+        else if (currentturn == 9)//뒷열전진2
+        {
+            currentturn = 10;
+            movemanager.StartMoveTurn();
+            turnend = true;
+        }
+        else if (currentturn == 10)//총격선택
+        {
+            currentturn = 11;
+            turnend = true;
+        }
+        else if(currentturn == 11)//상대총격선택
+        {
+            currentturn = 1;
+            turnend = true;
+        }
+    }
+    void Update()
+    {
+        if(turnend == true)
+        {
+            NextTurn();
+        }
+    }
+}
---
+++ DeathBoard/Assets/02.Scripts/fieldPreset/FieldReaction.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class FieldReaction : MonoBehaviour
+{
+    public FieldManager fieldManager;
+    public int[] fieldPosition;
+    GameObject thisField;
+    public GameObject readyCard;
+    public CardManager cardManager;
+    public CardSelecter cardSelecter;
+    public DeckManager deckmanager;
+    public EffectManager effectManager;
+    void OnMouseDown()
+    {
+        readyCard = fieldManager.readyCard;
+        Debug.LogWarning("Ŭ       ");
+        if (this.fieldPosition[0] < 2)
+        {
+            SetCardOnField(readyCard);
+        }
+    }
+
+    void SetCardOnField(GameObject setCard)
+    {
+        if (setCard != null && fieldManager.CurrntField[fieldPosition[0], fieldPosition[1]] == null && fieldManager.getReady() && TurnManager.currentturn == 2)
+        {
+            CardStateManager stateScript = setCard.GetComponent<CardStateManager>();
+            CardReaction reactionScript = setCard.GetComponent<CardReaction>();
+            GameObject[] thiscards = GameObject.FindGameObjectsWithTag(setCard.tag);
+            stateScript.thiscard.Position = fieldPosition;
+            DeckManager.CardArr[stateScript.thiscard.CardId].Position = fieldPosition;
+            fieldManager.CurrntField[DeckManager.CardArr[stateScript.thiscard.CardId].Position[0], DeckManager.CardArr[stateScript.thiscard.CardId].Position[1]] = stateScript.thiscard.CardId;
+            if (cardManager != null)
+            {
+                cardManager.PlayCard(setCard);
+            }
+            fieldManager.cardReady = false;
+            fieldManager.readyCard = null;
+            setCard.transform.position = thisField.transform.position;
+            setCard.transform.rotation = Quaternion.Euler(90f, transform.eulerAngles.y, transform.eulerAngles.z);
+            thiscards[1].transform.rotation = Quaternion.Euler(270f, transform.eulerAngles.y, 180f);
+            reactionScript.originalPosition = thisField.transform.position;
+<<<<<<< HEAD
+            reactionScript.originalPosition.y = reactionScript.originalPosition.y - 0.001f;
+            thiscards[1].transform.position = reactionScript.originalPosition;
+            reactionScript.originalPosition.y = reactionScript.originalPosition.y + 0.001f;
+=======
+            reactionScript.originalPosition.y = reactionScript.originalPosition.y-0.001f;
+            thiscards[1].transform.position = reactionScript.originalPosition;
+            reactionScript.originalPosition.y = reactionScript.originalPosition.y+0.001f;
+>>>>>>> e148a1e0d4fef6a49bbbc48fc361afb348430a1b
+            Destroy(setCard.GetComponent<CardReaction>());
+            cardSelecter = FindAnyObjectByType<CardSelecter>();
+            StartCoroutine(cardSelecter.CameraSmoothMoveRoutine());
+            effectManager.EffectCast(stateScript.thiscardID, 1);
+            //여기서 Effect Manager -> EffectAtSet 으로 접근
+        }
+        else return;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        fieldManager = FindAnyObjectByType<FieldManager>();
+        cardManager = FindAnyObjectByType<CardManager>();
+        fieldPosition = new int[2];
+        thisField = this.gameObject;
+    }
+
+    void Strat()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
