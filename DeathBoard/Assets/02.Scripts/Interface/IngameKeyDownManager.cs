1552b138fc234a546eb40881f56219d6c6e0fdf9
VIOLEILLICIT <133732909+VIOLEILLICIT@users.noreply.github.com>
2025-11-25 14:08:29 +0900

    dsfdsf

---
+++ DeathBoard/Assets/02.Scripts/Interface/IngameKeyDownManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EscMenuManager : MonoBehaviour
+{
+    private bool isActived = false;
+    public CardSelecter cardSelecter;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cardSelecter = FindAnyObjectByType<CardSelecter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isActived)
+            {
+                GameObject.Find("Canvas").transform.Find("Esc Screen").gameObject.SetActive(true);
+                isActived = true;
+            }
+            else
+            {
+                GameObject.Find("Canvas").transform.Find("Esc Screen").gameObject.SetActive(false);
+                isActived = false;
+            }
+
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(cardSelecter.CameraSmoothMoveRoutine());
+        }
+    }
+}
